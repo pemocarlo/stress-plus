582846bb120a8571b4484b127f12681e14d97566
Jonas Schuell <294-mu23niqa@users.noreply.mad-srv.informatik.uni-erlangen.de>
2020-05-15 12:38:24 +0200

    Resolve "Setup project"

---
+++ Code/frontend/src/components/dialpad/dialpad.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './dialpad.css';
+
+export default function Dialpad(props) {
+    const buttons = [...new Array(10)]
+            .map((_, i) => <button className={'button' + i} onClick={() => props.callback(i)} key={i}>{i}</button>);
+    return (
+         <div className='dialpad'>
+            {buttons}
+        </div>
+    );
+}
---
+++ Code/frontend/src/stress-app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Dialpad from './components/dialpad/dialpad';
+import './stress-app.css';
+
+export default function StressApp() {
+  return (
+    <div className="StressApp">
+      <h1>Stress App</h1>
+      <Dialpad callback={(c) => console.log(c)}/>
+    </div>
+  );
+}
