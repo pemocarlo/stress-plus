da2f8f0688edce5975290be407db1c67426d3623
Jonas Schüll <jonas.schuell@fau.de>
2020-05-18 08:53:40 +0200

    Revert "Changes to stress-app.js to include levelbar."
    This reverts commit a56f82d53468cba003d357c75e722e9daef6334c.

--- Code/frontend/src/stress-app.js
+++ Code/frontend/src/stress-app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Dialpad from 'components/dialpad/dialpad';
-import Levelbar from 'components/levelbar/Levelbar';
 import 'stress-app.css';
 
 export default function StressApp() {
@@ -9,7 +8,6 @@ export default function StressApp() {
     <div className="StressApp">
       <h1>Stress App</h1>
       <Dialpad callback={(c) => console.log(c)}/>
-	    <Levelbar></Levelbar>
     </div>
   );
 }
