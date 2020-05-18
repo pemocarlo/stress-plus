28ecbf50b26a210499c17339b24412077c8bf4df
Carlos Perez <carlosperezmolano@gmail.com>
2020-05-18 15:43:13 +0200

    Add basic layout structure
    This commit also reorganizes the components and adds the whole
    arithmetic test into a new one that is loaded in StressApp.js

--- Code/frontend/src/stress-app.js
+++ Code/frontend/src/stress-app.js
@@ -1,15 +1,8 @@
-import React from 'react';
+import React from "react";
 
-import Dialpad from 'components/dialpad/dialpad';
-import ProgressBar from 'components/progressBar/ProgressBar';
-import 'stress-app.css';
+import ArithmeticTest from "components/ArithmeticTest/ArithmeticTest";
+import "./stress-app.css";
 
 export default function StressApp() {
-  return (
-    <div className="StressApp">
-      <h1>Stress App</h1>
-      <Dialpad callback={(c) => console.log(c)}/>
-      <ProgressBar percentage={60} />
-    </div>
-  );
+  return <ArithmeticTest />;
 }
