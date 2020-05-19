590b8969351065fcd11c6e44e7761f582e345d80
Jonas Schüll <jonas.schuell@fau.de>
2020-05-19 15:01:51 +0200

    Try react router

--- Code/frontend/src/stress-app.js
+++ Code/frontend/src/stress-app.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {BrowserRouter, Switch, Route, Link} from "react-router-dom";
 
 import ArithmeticTest from "components/ArithmeticTest/ArithmeticTest";
 import "./stress-app.css";
@@ -6,5 +7,17 @@ import "./stress-app.css";
 const props = {seconds: 3};
 
 export default function StressApp() {
-  return <ArithmeticTest {...props} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <h1>Hallo Mist Test</h1>
+          <Link to="/mist-test">Start test</Link>
+        </Route>
+        <Route path="/mist-test">
+          <ArithmeticTest {...props}/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
