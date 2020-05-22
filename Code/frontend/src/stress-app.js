4f951de2090aa1e425e46fb69bb09eb01c003dbb
Carlos Alberto Perez Molano <carlos.a.perez@fau.de>
2020-05-22 09:10:55 +0200

    Refactor ArithmeticTest with useReducer hook
    This change improves the dependencies in useEffect hooks and simplify
    the code by separating state management in its own function

--- Code/frontend/src/stress-app.js
+++ Code/frontend/src/stress-app.js
@@ -3,7 +3,7 @@ import React from "react";
 import ArithmeticTest from "components/ArithmeticTest/ArithmeticTest";
 import "./stress-app.css";
 
-const props = {seconds: 5, renderStepProgressms: 50};
+const props = {seconds: 3};
 
 export default function StressApp() {
   return <ArithmeticTest {...props} />;
