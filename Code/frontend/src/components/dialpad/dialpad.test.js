f2426c9356d98f078a91def4d8d956a3e501101a
Abinaya Aravindan <299-yq60egil@users.noreply.mad-srv.informatik.uni-erlangen.de>
2020-05-29 10:49:04 +0200

    Test for all Buttons_Dialpad

---
+++ Code/frontend/src/components/dialpad/dialpad.test.js
@@ -0,0 +1,85 @@
+import React from "react";
+import {render, screen} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import Dialpad from "./dialpad";
+
+test("Button 0", () => {
+  expect.assertions(2);
+  render(<Dialpad callback={(c) => expect(c).toEqual(0)} />);
+  const btn = screen.getByText(/0/i);
+  expect(btn).toBeInTheDocument();
+  userEvent.click(btn);
+});
+
+test("Button 1", () => {
+  expect.assertions(2);
+  render(<Dialpad callback={(c) => expect(c).toEqual(1)} />);
+  const btn = screen.getByText(/1/i);
+  expect(btn).toBeInTheDocument();
+  userEvent.click(btn);
+});
+
+test("Button 2", () => {
+  expect.assertions(2);
+  render(<Dialpad callback={(c) => expect(c).toEqual(2)} />);
+  const btn = screen.getByText(/2/i);
+  expect(btn).toBeInTheDocument();
+  userEvent.click(btn);
+});
+
+test("Button 3", () => {
+  expect.assertions(2);
+  render(<Dialpad callback={(c) => expect(c).toEqual(3)} />);
+  const btn = screen.getByText(/3/i);
+  expect(btn).toBeInTheDocument();
+  userEvent.click(btn);
+});
+
+test("Button 4", () => {
+  expect.assertions(2);
+  render(<Dialpad callback={(c) => expect(c).toEqual(4)} />);
+  const btn = screen.getByText(/4/i);
+  expect(btn).toBeInTheDocument();
+  userEvent.click(btn);
+});
+
+test("Button 5", () => {
+  expect.assertions(2);
+  render(<Dialpad callback={(c) => expect(c).toEqual(5)} />);
+  const btn = screen.getByText(/5/i);
+  expect(btn).toBeInTheDocument();
+  userEvent.click(btn);
+});
+
+test("Button 6", () => {
+  expect.assertions(2);
+  render(<Dialpad callback={(c) => expect(c).toEqual(6)} />);
+  const btn = screen.getByText(/6/i);
+  expect(btn).toBeInTheDocument();
+  userEvent.click(btn);
+});
+
+test("Button 7", () => {
+  expect.assertions(2);
+  render(<Dialpad callback={(c) => expect(c).toEqual(7)} />);
+  const btn = screen.getByText(/7/i);
+  expect(btn).toBeInTheDocument();
+  userEvent.click(btn);
+});
+
+test("Button 8", () => {
+  expect.assertions(2);
+  render(<Dialpad callback={(c) => expect(c).toEqual(8)} />);
+  const btn = screen.getByText(/8/i);
+  expect(btn).toBeInTheDocument();
+  userEvent.click(btn);
+});
+
+test("Button 9", () => {
+  expect.assertions(2);
+  render(<Dialpad callback={(c) => expect(c).toEqual(9)} />);
+  const btn = screen.getByText(/9/i);
+  expect(btn).toBeInTheDocument();
+  userEvent.click(btn);
+});
