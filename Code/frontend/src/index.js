7c2f6bd118c73b2f411d1e6f5cee6163e8611401
Antonia Ohlmann <293-vi37nyso@users.noreply.mad-srv.informatik.uni-erlangen.de>
2020-06-02 13:44:20 +0200

    Resolve "Bootstrap: index.scss"

--- Code/frontend/src/index.js
+++ Code/frontend/src/index.js
@@ -2,7 +2,6 @@ import React, {Suspense} from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
 
-import "./index.css";
 import "./index.scss";
 import StressApp from "./stress-app";
 import * as serviceWorker from "./service-worker";
