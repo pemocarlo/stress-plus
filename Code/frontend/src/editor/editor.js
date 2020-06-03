6c455887fbeab74b5e040bb35509637f5ca5543d
Jonas Schuell <294-mu23niqa@users.noreply.mad-srv.informatik.uni-erlangen.de>
2020-06-03 17:24:50 +0200

    Add toolbar to editor

--- Code/frontend/src/editor/editor.js
+++ Code/frontend/src/editor/editor.js
@@ -1,99 +1,113 @@
 import qs from "qs";
-import React, {useState} from "react";
+import React, {useState, useCallback} from "react";
 import {DragDropContext} from "react-beautiful-dnd";
-import {useTranslation} from "react-i18next";
-
-import Column from "editor/column";
 import Button from "react-bootstrap/Button";
+import {useTranslation} from "react-i18next";
+import {v4 as uuid} from "uuid";
+
+import Pipeline from "editor/pipeline";
+import Toolbar from "editor/toolbar";
 import "./editor.scss";
 
-const initialData = {
-  screens: {
-    screen1: {
-      type: "mathTest",
-      name: "math test 1",
-      id: "screen1",
-      title: "Math test 1",
-    },
-    screen2: {
-      type: "message",
-      name: "message 1",
-      id: "screen2",
-      title: "message 1",
-    },
-    screen3: {
-      type: "mathTest",
-      name: "math test 2",
-      id: "screen3",
-      title: "Math test 2",
-    },
-    screen4: {
-      type: "message",
-      name: "message 2",
-      id: "screen4",
-      title: "message 2: Test finished!",
-    },
+const TOOLBAR = [
+  {
+    type: "mathTest",
+    id: "mathTest",
+    name: "Math Test",
+    title: "Math Test",
   },
+  {
+    type: "message",
+    id: "message",
+    name: "Message",
+    title: "Message",
+  },
+];
 
-  columns: [
-    {
-      id: "column1",
-      title: "screens",
-      screenList: ["screen1", "screen2", "screen3", "screen4"],
-    },
-  ],
+const initialData = [
+  {
+    type: "mathTest",
+    name: "math test 1",
+    id: uuid(),
+    title: "Math test 1",
+  },
+  {
+    type: "message",
+    name: "message 1",
+    id: uuid(),
+    title: "message 1",
+  },
+  {
+    type: "mathTest",
+    name: "math test 2",
+    id: uuid(),
+    title: "Math test 2",
+  },
+  {
+    type: "message",
+    name: "message 2",
+    id: uuid(),
+    title: "message 2: Test finished!",
+  },
+];
+
+function reorder(list, startIndex, endIndex) {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+  return result;
+}
+
+const copy = (source, destination, droppableSource, droppableDestination) => {
+  const item = source[droppableSource.index];
+  destination.splice(droppableDestination.index, 0, {...item, id: uuid()});
+  return destination;
 };
 
 export default function Editor() {
   const {t} = useTranslation();
   const [link, setLink] = useState("");
-  const [state, setState] = useState(initialData);
+  const [pipeline, setPipeline] = useState(initialData);
 
-  const onDragEnd = (result) => {
-    const {destination, source, draggableId} = result;
+  const onDragEnd = useCallback(
+    (result) => {
+      const {destination, source} = result;
 
-    // if destination is not a droppable
-    if (!destination) {
-      return;
-    }
+      // if destination is not a droppable
+      if (!destination) {
+        return;
+      }
 
-    // checks case where user drops to the same position and do nothing
-    if (destination.droppableId === source.droppableId && destination.index === source.index) {
-      return;
-    }
+      // checks case where user drops to the same position and do nothing
+      if (destination.droppableId === source.droppableId && destination.index === source.index) {
+        return;
+      }
 
-    // keep track of reordering of screens in the column
-    const column = state.columns.find((obj) => obj.id === source.droppableId);
-    const newScreenList = Array.from(column.screenList);
-
-    newScreenList.splice(source.index, 1);
-    newScreenList.splice(destination.index, 0, draggableId);
-
-    const newColumn = {...column, screenList: newScreenList};
-
-    const newState = {
-      ...state,
-      columns: [newColumn],
-    };
-
-    setState(newState);
-  };
+      switch (source.droppableId) {
+        case "PIPELINE":
+          setPipeline((pipeline) => reorder(pipeline, source.index, destination.index));
+          break;
+        case "TOOLBAR":
+          setPipeline((pipeline) => copy(TOOLBAR, pipeline, source, destination));
+          break;
+        default:
+          break;
+      }
+    },
+    [setPipeline]
+  );
 
   const onGenerateLink = () => {
-    const screens = state.columns[0].screenList.map((screenId) => state.screens[screenId]);
-    const link = qs.stringify({screens}, {allowDots: true});
+    const link = qs.stringify({screens: pipeline}, {allowDots: true});
+    console.log(`Link length: ${link.length}`);
     setLink(`localhost:3000/executor?${link}`);
   };
 
   return (
     <div className="Editor">
       <DragDropContext onDragEnd={onDragEnd}>
-        <div className="columns">
-          {state.columns.map((column) => {
-            const currentScreens = column.screenList.map((screenName) => state.screens[screenName]);
-            return <Column key={column.id} column={column} screens={currentScreens} />;
-          })}
-        </div>
+        <Toolbar id="TOOLBAR" items={TOOLBAR} />
+        <Pipeline id="PIPELINE" items={pipeline} />
       </DragDropContext>
       <Button onClick={onGenerateLink}>{t("editor.generateLink")}</Button>
       <input type="text" value={link} className="link-box" readOnly></input>
---
+++ Code/frontend/src/editor/toolbar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import {Droppable, Draggable} from "react-beautiful-dnd";
+
+import "./toolbar.scss";
+
+export default function Toolbar(props) {
+  return (
+    <div className="toolbar">
+      <h3>Toolbar</h3>
+      <Droppable droppableId={props.id} direction="horizontal" isDropDisabled={true}>
+        {(provided) => (
+          <div className="items" ref={provided.innerRef}>
+            {props.items.map((item, index) => (
+              <ToolbarDraggable {...item} key={item.id} index={index} />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </div>
+  );
+}
+
+function ToolbarDraggable(props) {
+  return (
+    <Draggable key={props.id} draggableId={props.id} index={props.index}>
+      {(provided, snapshot) => (
+        <React.Fragment>
+          <div
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            className="toolbar-draggable"
+          >
+            <ToolbarItem {...props} />
+          </div>
+          {snapshot.isDragging && (
+            <div className="toolbar-draggable toolbar-item-copy">
+              <ToolbarItem {...props} />
+            </div>
+          )}
+        </React.Fragment>
+      )}
+    </Draggable>
+  );
+}
+
+function ToolbarItem(props) {
+  return <div className="toolbar-item">{props.name}</div>;
+}
