a976a5ebcaabe12f889bfd7d41cc0dbeef4fb494
Jonas Schüll <jonas.schuell@fau.de>
2020-06-05 09:25:50 +0200

    Fix screen registry

--- Code/frontend/src/screens/screen-registry.js
+++ Code/frontend/src/screens/screen-registry.js
@@ -1,17 +1,27 @@
+import React from "react";
+
 import MathComponent from "./math/math-component";
 import MessageComponent from "./message/message-component";
 
 export default {
   mathTest: {
-    component: MathComponent,
+    component: MathTestScreen,
     initialSettings: {
       title: "Math Test",
     },
   },
   message: {
-    component: MessageComponent,
+    component: MessageScreen,
     initialSettings: {
       title: "Message",
     },
   },
 };
+
+function MathTestScreen(props) {
+  return <MathComponent {...props} />;
+}
+
+function MessageScreen(props) {
+  return <MessageComponent {...props} />;
+}
