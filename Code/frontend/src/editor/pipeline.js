d7c7faf67c97f983b51543f9c12afa80fa0331f6
Antonia Ohlmann <293-vi37nyso@users.noreply.mad-srv.informatik.uni-erlangen.de>
2020-06-08 08:50:47 +0200

    Resolve "Bootstrap: Pipeline Settings"

--- Code/frontend/src/editor/pipeline.js
+++ Code/frontend/src/editor/pipeline.js
@@ -1,10 +1,13 @@
 import React from "react";
 import {Droppable, Draggable} from "react-beautiful-dnd";
 import Button from "react-bootstrap/Button";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
 import {useTranslation} from "react-i18next";
 
 import screenRegistry from "../screens/screen-registry";
 import overlayRegistry from "../overlays/overlay-registry";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import "./pipeline.scss";
 
 export default function Pipeline(props) {
@@ -64,10 +67,30 @@ function getSettingsComponent(props) {
 function PipelineItem(props) {
   const {t} = useTranslation();
   return (
-    <div className="item">
-      {t(`editor.${props.dndType}.items.${props.type}.name`)}
-      <Button onClick={props.onDelete}>X</Button>
-      {getSettingsComponent(props)}
-    </div>
+    <Card className="card">
+      <Card.Header className="cardHead-1">
+        <div className="col-9">{t(`editor.${props.dndType}.items.${props.type}.name`)}</div>
+        <div className="col-3">
+          <Button className="button" onClick={props.onDelete}>
+            <FontAwesomeIcon icon="trash-alt" />
+          </Button>
+        </div>
+      </Card.Header>
+      <Card.Body className="cardBody">
+        <Accordion defaultActiveKey="0">
+          <Card className="card">
+            <Card.Header className="cardHead">
+              <Accordion.Toggle as={Button} variant="link" eventKey="1" className="cardLink">
+                Settings
+                <FontAwesomeIcon icon="cog" />
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="1">
+              <Card.Body className="cardBodyCollapse">{getSettingsComponent(props)}</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+      </Card.Body>
+    </Card>
   );
 }
