bd00a763c0bf90ae7499c48cbd24ab48ed1e1b71
Abinaya Aravindan <299-yq60egil@users.noreply.mad-srv.informatik.uni-erlangen.de>
2020-06-20 10:35:37 +0200

    Waiting screen

--- Code/frontend/src/screens/screen-registry.js
+++ Code/frontend/src/screens/screen-registry.js
@@ -4,6 +4,8 @@ import ArithmeticTest from "./math/arithmetic-test";
 import MathSettings from "./math/math-settings";
 import MessageComponent from "./message/message-component";
 import MessageSettings from "./message/message-settings";
+import WaitingComponent from "./waiting-screen/waiting-component";
+import WaitingSettings from "./waiting-screen/waiting-settings";
 
 export default {
   mathTest: {
@@ -29,6 +31,16 @@ export default {
     },
     settingsComponent: MessageScreenSettings,
   },
+
+  waiting: {
+    component: WaitingScreen,
+    initialSettings: {
+      title: "Connecting...",
+      message: "Please wait until the host lets you in!",
+      wait_time: 5,
+    },
+    settingsComponent: WaitingScreenSettings,
+  },
 };
 
 function ArithmeticTestScreen(props) {
@@ -45,4 +57,12 @@ function MathTestScreenSettings(props) {
 
 function MessageScreenSettings(props) {
   return <MessageSettings {...props} />;
+}
+
+function WaitingScreen(props) {
+  return <WaitingComponent {...props} />;
+}
+
+function WaitingScreenSettings(props) {
+  return <WaitingSettings {...props} />;
 }
