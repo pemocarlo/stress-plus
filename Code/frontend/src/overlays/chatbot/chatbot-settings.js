e2cca4e112f890564c968956d64254c9b4d85ad3
Carlos Alberto Perez Molano <carlos.a.perez@fau.de>
2020-06-25 10:48:20 +0200

    Resolve "Chatbot Functionality"

---
+++ Code/frontend/src/overlays/chatbot/chatbot-settings.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {useTranslation} from "react-i18next";
+
+import NumberInput from "components/number-input/number-input";
+
+export default function ScreenFrezzeSettings(props) {
+  const {t} = useTranslation();
+  const onChange = (name, value) => props.updateSettings(props.id, name, value);
+
+  return (
+    <div>
+      <NumberInput
+        name="showAt"
+        label={t("editor.overlay.items.chatbot.startTime")}
+        value={props.startTime}
+        onChange={onChange}
+        required
+        min={0}
+      />
+    </div>
+  );
+}
