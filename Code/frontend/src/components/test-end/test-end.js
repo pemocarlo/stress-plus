928a26d78d159b8abdfa80e62f37a37a77d024b8
Jonas Schüll <jonas.schuell@fau.de>
2020-06-25 11:08:43 +0200

    Remove back to settings button from test-end

--- Code/frontend/src/components/test-end/test-end.js
+++ Code/frontend/src/components/test-end/test-end.js
@@ -1,27 +1,16 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import React from "react";
-import Button from "react-bootstrap/Button";
 import {useTranslation} from "react-i18next";
-import {useHistory} from "react-router-dom";
 
 import MainLayout from "components/main-layout/main-layout";
 import "./test-end.scss";
 
 export default function TestEnd() {
   const {t} = useTranslation();
-  const history = useHistory();
   return (
     <MainLayout>
       <div className="container" id="test-end">
         <div className="row">
           <div className="message">{t("end.message")}</div>
-        </div>
-        <div className="row">
-          <div className="buttonSettings">
-            <Button onClick={() => history.push("/")}>
-              {t("end.button")} <FontAwesomeIcon icon="cog" />
-            </Button>
-          </div>
         </div>
       </div>
     </MainLayout>
