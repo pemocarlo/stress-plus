b7cd1ddc3ca9fafa2843e37f0181b7b1373636aa
Jonas Schüll <294-mu23niqa@users.noreply.mad-srv.informatik.uni-erlangen.de>
2020-06-29 18:14:25 +0200

    Fix caching of translation files

--- Code/frontend/src/stress-app.js
+++ Code/frontend/src/stress-app.js
@@ -6,6 +6,11 @@ import TestEnd from "components/test-end/test-end";
 import Editor from "editor/editor";
 import PipelineExecutor from "executor/pipeline-executor";
 
+export const VERSION = process.env.REACT_APP_VERSION;
+export const BUILD_TIME = process.env.REACT_APP_BUILD_TIME;
+export const IS_PRODUCTION = process.env.NODE_ENV === "production";
+export const IS_DEVELOPMENT = process.env.NODE_ENV === "development";
+
 export default function StressApp() {
   return (
     <Switch>
