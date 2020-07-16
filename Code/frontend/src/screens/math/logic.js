fd4d9b3a5bc787e785a8e2a3f8e52c97de5cd3bd
Thea Vogel <307-ra10wiby@users.noreply.mad-srv.informatik.uni-erlangen.de>
2020-07-16 10:01:23 +0200

    Resolve "3 times wrong/right answer (logic)"

--- Code/frontend/src/screens/math/logic.js
+++ Code/frontend/src/screens/math/logic.js
@@ -6,13 +6,14 @@ export const RESULT_WRONG = 2;
 export const RESULT_TIMEOUT = 3;
 
 export function getInitialState(settings) {
-  // console.log(settings);
   return {
     result: RESULT_NONE,
     expression: "",
     mathResult: "",
     progressPercentage: 0,
     correctAnswers: 0,
+    correctDifficulty: 0,
+    incorrectDifficulty: 0,
     totalAnswers: 0,
     initialQuestionTime: new Date(),
     finalQuestionTime: 0,
@@ -25,6 +26,7 @@ export function getInitialState(settings) {
     isControl: settings.isControl,
     testTotalTime: settings.testTotalTime,
     difficulty: settings.difficulty,
+    counters: [0, 0, 0, 0, 0],
   };
 }
 
@@ -41,6 +43,8 @@ export function mainReducer(state, action) {
           waiting: true,
           result: RESULT_CORRECT,
           correctAnswers: state.correctAnswers + 1,
+          correctDifficulty: state.correctDifficulty + 1,
+          incorrectDifficulty: 0,
           totalAnswers: state.totalAnswers + 1,
           averageScore: state.averageScore + Math.exp(-0.075 * state.correctAnswers),
           yourScore: Math.floor(((state.correctAnswers + 1) / (state.totalAnswers + 1)) * 100),
@@ -51,6 +55,8 @@ export function mainReducer(state, action) {
           waiting: true,
           result: RESULT_WRONG,
           incorrectAnswers: state.incorrectAnswers + 1,
+          incorrectDifficulty: state.incorrectDifficulty + 1,
+          correctDifficulty: 0,
           totalAnswers: state.totalAnswers + 1,
           averageScore: state.averageScore - 1,
           yourScore: Math.floor((state.correctAnswers / (state.totalAnswers + 1)) * 100),
@@ -63,6 +69,7 @@ export function mainReducer(state, action) {
         waiting: true,
         result: RESULT_TIMEOUT,
         incorrectAnswers: state.incorrectAnswers + 1,
+        correctDifficulty: 0,
         totalAnswers: state.totalAnswers + 1,
         averageScore: state.averageScore - 1,
         yourScore: Math.floor((state.correctAnswers / (state.totalAnswers + 1)) * 100),
@@ -72,9 +79,11 @@ export function mainReducer(state, action) {
     case "firstQuestion": {
       const finalQuestionTime = new Date();
       finalQuestionTime.setSeconds(finalQuestionTime.getSeconds() + state.answerTimeout);
-      const mathQuestions = mathGenerator(state.difficulty);
-      const expression = mathQuestions[0][0];
-      const result = mathQuestions[0][1];
+      const mathQuestions = mathGenerator();
+      const expression = mathQuestions[state.difficulty][0][0];
+      const result = mathQuestions[state.difficulty][0][1];
+      const newCounters = [0, 0, 0, 0, 0];
+      newCounters[state.difficulty] = 1;
       return {
         ...state,
         waiting: false,
@@ -84,25 +93,39 @@ export function mainReducer(state, action) {
         finalQuestionTime: finalQuestionTime,
         result: RESULT_NONE,
         mathQuestions: mathQuestions,
-        counter: 1,
+        counters: newCounters,
       };
     }
 
     case "newQuestion": {
       const finalQuestionTime = new Date();
       finalQuestionTime.setSeconds(finalQuestionTime.getSeconds() + state.answerTimeout);
-      const expression = state.mathQuestions[state.counter][0];
-      const result = state.mathQuestions[state.counter][1];
-      return {
+      const newState = {
         ...state,
         initialQuestionTime: new Date(),
         waiting: false,
-        expression: expression,
-        mathResult: result,
         progressPercentage: 0,
         finalQuestionTime: finalQuestionTime,
         result: RESULT_NONE,
-        counter: (state.counter + 1) % state.mathQuestions.length,
+      };
+      if (state.correctDifficulty === 3 && state.difficulty < 4) {
+        newState.difficulty = state.difficulty + 1;
+        newState.correctDifficulty = 0;
+      } else if (state.incorrectDifficulty === 3 && state.difficulty > 0) {
+        newState.difficulty = state.difficulty - 1;
+        newState.incorrectDifficulty = 0;
+      }
+      const questions = state.mathQuestions[newState.difficulty];
+      const questionIndex = state.counters[newState.difficulty];
+      const expression = questions[questionIndex][0];
+      const result = questions[questionIndex][1];
+      const newCounters = [...state.counters];
+      newCounters[newState.difficulty] = (questionIndex + 1) % questions.length;
+      return {
+        ...newState,
+        expression: expression,
+        mathResult: result,
+        counters: newCounters,
       };
     }
     case "updateProgressPercentage": {
--- Code/frontend/src/services/math-generator.test.js
+++ Code/frontend/src/services/math-generator.test.js
@@ -9,21 +9,21 @@ function assertQuestions(questions) {
 }
 
 test("math question correctness: difficulty0", () => {
-  assertQuestions(mathGenerator(0));
+  assertQuestions(mathGenerator()[0]);
 });
 
 test("math question correctness: difficulty1", () => {
-  assertQuestions(mathGenerator(1));
+  assertQuestions(mathGenerator()[1]);
 });
 
 test("math question correctness: difficulty2", () => {
-  assertQuestions(mathGenerator(2));
+  assertQuestions(mathGenerator()[2]);
 });
 
 test("math question correctness: difficulty3", () => {
-  assertQuestions(mathGenerator(3));
+  assertQuestions(mathGenerator()[3]);
 });
 
 test("math question correctness: difficulty4", () => {
-  assertQuestions(mathGenerator(4));
+  assertQuestions(mathGenerator()[4]);
 });
